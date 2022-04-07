c7ebe1bc6731ab3b39c947589b54482681e33a61
Omosquary <iamomosquare@gmail.com>
2022-04-07 04:48:33 -0700

    Mandatory Task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  *main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
-	printf ("\" with proper grammar, but the outcome is a piece of art," );
+	printf("with proper grammar, but the outcome is a piece of art,\n" );
 	return (0);
 }
