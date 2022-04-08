4d60ea2bd0d9002e2fa430d9f48e5c68695fdfc9
Omotayo Olamilekan Omotosho <iamomosquare@gmail.com>
2022-04-08 17:30:25 +0100

    Mandatory Task

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -10,8 +11,9 @@ int main(void)
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
+	{
 		putchar(c);
-		putchar('\n');
-
-return (0);
+	}
+	putchar('\n');
+	return (0);
 }
