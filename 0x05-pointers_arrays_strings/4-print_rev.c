231a361a04f8c6f8a9856e5b1916b8b96f63558e
Omosquary <iamomosquare@gmail.com>
2022-04-20 06:17:47 -0700

    Printing in Reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,9 +8,9 @@ void print_rev(char *s)
 {
 	int len;
 
-	//for (len = 0; s[len] != '\0'; len++)
-	//{
-	//}
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
 	for (len = len - 1; len >= 0; len--)
 	{
 		_putchar(s[len]);
