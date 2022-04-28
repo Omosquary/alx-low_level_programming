06bacb26280cb53d69cb8f1da93fa9ad57712a26
Omotayo Olamilekan Omotosho <iamomosquare@gmail.com>
2022-04-28 10:55:56 +0100

    Prints a String in Reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * _print_rev_recursion - prints a string in reverse
+ * _print_rev_recursion -  prints a string, in reverse.
  * @s: pointer to string
  * Return: nothing
- */
-
+ **/
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
