f0861cba39fa5c33c9ed1ab8c8eabdba1a0735b4
Omotayo Olamilekan Omotosho <iamomosquare@gmail.com>
2022-04-28 10:57:05 +0100

    Prints a string, followed by a new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * _pus_recursion - prints a string, followed by a new line
- * @s: pointer to string
- * Return: nothing
- */
-
+  * _puts_recursion - prints a string, followed by a new line
+  * @s: pointer to string
+  * Return: nothing
+  **/
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
