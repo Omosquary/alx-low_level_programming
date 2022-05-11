c48b72b5b2fbe1f760c30871a7564c89d5210d0c
Omosquary <iamomosquare@gmail.com>
2022-05-11 04:40:29 -0700

    Structure Mandatory Task 0 correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG
 #define DOG
-
 /**
  * struct dog - is a structure for a dog object
  * @name: is the name element
@@ -10,7 +9,12 @@
 struct dog
 {
 	char *name;
-	float *age;
+	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
