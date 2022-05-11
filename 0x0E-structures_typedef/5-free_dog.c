fd2096923f6fd67b162d142fa286031bbee62a56
Omosquary <iamomosquare@gmail.com>
2022-05-11 05:34:12 -0700

    Structure Mandatory Task 5 Correction

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,13 +1,13 @@
 #include "dog.h"
-#include <stdlib>
+#include <stdlib.h>
 /**
  * free_dog - is a function that frees memory of dog
- * @d: is a pointer to dog
+ * @d: is a pointer to a dog of type dog_t
  * Return: void
  */
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
+	if (d != NULL)
 	{
 		free(d->name);
 		free(d->owner);
