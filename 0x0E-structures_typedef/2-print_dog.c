6c1fb19b7c22a7dabb1d580186142d7a4c97a058
Omosquary <iamomosquare@gmail.com>
2022-05-11 05:05:50 -0700

    Structure Mandatory Task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,13 +13,23 @@ void print_dog(struct dog *d)
 	else
 	{
 		if (d->name != NULL)
+		{
 			printf("Name: %s\n", d->name);
+		}
 		else
-			printf("Name: (null)\n");
+		{
+			printf("Name: (nil)\n");
+		}
+
 		printf("Age: %f\n", d->age);
+
 		if (d->owner != NULL)
+		{
 			printf("Owner: %s\n", d->owner);
+		}
 		else
-			printf("Owner: (null)\n");
+		{
+			printf("Owner: (nil)\n");
+		}
 	}
 }
