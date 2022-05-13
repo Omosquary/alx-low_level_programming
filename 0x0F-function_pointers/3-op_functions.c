717a5a4b300dc283f0e8ec9cc78f3cd4f6b6390d
Omosquary <iamomosquare@gmail.com>
2022-05-13 05:26:09 -0700

    Task 3 Correction

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -4,15 +4,14 @@
  * op_add - is a function that performs addition
  * @a: sumnd 1
  * @b: sumnd 2
- * Return: a+b
+ * Return: a + b
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
 /**
- * op_sub - is a function that performs substration
+ * op_sub - is a function that performs subtraction
  * @a: minuend
  * @b: sustrnd
  * Return: a - b
@@ -21,7 +20,6 @@ int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
 /**
  * op_mul - is a function that performs multiplication
  * @a: multp 1
@@ -32,7 +30,6 @@ int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
 /**
  * op_div - is a function that performs division
  * @a: divisr
@@ -41,22 +38,25 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-	if (b == 0)
+	if (b)
 	{
-		return (-1);
+		return (a / b);
 	}
-	return (a / b);
+	printf("Error\n");
+	exit(100);
 }
-
 /**
- * op_mod - calculates mod of two ints
- * @a: number 1
- * @b: number 2
- * Return: mod of 1, 2
- **/
+ * op_mod - is a function that performs modulo
+ * @a: vaper 1
+ * @b: vaper 2
+ * Return: a % b
+ */
 int op_mod(int a, int b)
 {
-	if (b == 0)
-		return (-1);
-	return (a % b);
+	if (b)
+	{
+		return (a % b);
+	}
+	printf("Error\n");
+	exit(100);
 }
