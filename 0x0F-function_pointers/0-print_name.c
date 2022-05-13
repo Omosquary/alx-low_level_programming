fd27d36807178774d869750efef36f47df32dd54
Omosquary <iamomosquare@gmail.com>
2022-05-13 03:59:47 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: pointer to function that returns nothing
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+		f(name);
+	}
+}
