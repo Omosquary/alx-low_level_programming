c25608eb9b2991db03ad20e77ca3deb7d916b90a
Omosquary <iamomosquare@gmail.com>
2022-05-13 04:11:16 -0700

    Update HeaderFile

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
 #ifndef function_pointers_h
 #define function_pointers_h
-
+#include <stddef.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
