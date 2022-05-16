875cd36372dcb1222a2d32932ce0bc2843ce15db
Omosquary <iamomosquare@gmail.com>
2022-05-16 04:18:14 -0700

    Task 2 Mandatory Task

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdrag.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_strings - is a function that prints strings
+ * @n: is the input
+ * @separator: seperator input
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+	char *s;
+
+	va_start(args, n);
+	i = 0;
+	while (i < n)
+	{
+		s = va_arg(args, char *);
+		if (s == NULL)
+			s = "(nil)";
+		printf("%s", s);
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+		i++;
+	}
+		va_end(args);
+		printf("\n");
+}
