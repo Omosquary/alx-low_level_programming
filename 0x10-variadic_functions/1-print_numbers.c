da20fa3f508cb3a32f2a7c8afa654f410c173782
Omosquary <iamomosquare@gmail.com>
2022-05-16 04:20:12 -0700

    Task 1 Mandatory Correction

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,26 +1,26 @@
-#include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
 /**
  * print_numbers - is a function that prints numbers
- * @n: is the integer input
- * @seperator: seperator input
- * Return: nothing
+ * @n: is the int input
+ * @separator: separator input
+ * Return: numbers entered as args to the function
  */
-
-void print_numbers(const char *seperator, const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list numbers;
 	unsigned int i;
+	va_list args;
 
-	va_start(numbers, n);
-	for (i = 0; i < n; i++)
+	va_start(args, n);
+	i = 0;
+	while (i < n)
 	{
-		printf("%d", va_arg(numbers, unsigned int));
-		if (i < (n - 1) && separator != NULL)
+		printf("%d", va_arg(args, int));
+		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
+		i++;
 	}
-	va_end(numbers);
-	printf("\n");
+		va_end(args);
+		printf("\n");
 }
