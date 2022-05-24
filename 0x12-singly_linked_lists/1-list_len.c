cb3ecf7062aeffbd9f014bb34f760c6e313b6d25
Omosquary <iamomosquare@gmail.com>
2022-05-24 06:15:11 -0700

    Updating Task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,12 +10,12 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t count = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
 		h = h->next;
-		count++;
+		nodes++;
 	}
-	return (count);
+	return (nodes);
 }
