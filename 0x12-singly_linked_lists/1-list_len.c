8b18a0e264d75f61f4ea6f3476e837feb1ca7593
Omosquary <iamomosquare@gmail.com>
2022-05-24 06:13:33 -0700

    Mandatory Task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * list_len - is a function that counts the elements of a list_t file
+ * @h: is the string input
+ * Return: number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
